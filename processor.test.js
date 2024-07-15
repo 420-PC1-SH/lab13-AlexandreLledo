1adf12bf2037e3e270a6de70a7ae52744c04e2e0
AlexandreLledo <148722057+AlexandreLledo@users.noreply.github.com>
2024-07-15 09:09:04 -0400

    requis 1 contains > fail

--- processor.test.js
+++ processor.test.js
@@ -31,4 +31,9 @@ describe("transmission processor", function () {
         const expectedError = new Error('Data is invalid ; should contain "<"');
         expect(() => { processor("9701::489584872710>"); }).toThrow(expectedError);
     });
+
+    test("throws error if '>' not found", function () {
+        const expectedError = new Error('Data is invalid ; should contain ">"');
+        expect(() => { processor("9701::<489584872710"); }).toThrow(expectedError);
+    });
 });
